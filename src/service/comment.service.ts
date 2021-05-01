04a22dd03b8657025f7caa3f91a9713043d072cd
Johnny Kung <kjohnathan.engi@gmail.com>
2021-04-30 21:27:54 -0700

    added comment service

--- src/service/comment.service.ts
+++ src/service/comment.service.ts
@@ -6,8 +6,13 @@ class CommentService {
             const newComment = new CommentModel(userId, comment);
 
             const result = await newComment.create();
+            if (result.status === 200) {
+                return result;
+            } else {
+                throw new Error();
+            }
         } catch(err) {
-            
+            return err
         }
     }
 }
