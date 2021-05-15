bdb64e3cf1d8e5b96ae6cd55c2e69713ca89b005
Sean Luo <shawnluo415@gmail.com>
2021-05-15 02:25:45 -0700

    minor fix

--- src/middleware/errorHandling.middleware.ts
+++ src/middleware/errorHandling.middleware.ts
@@ -2,7 +2,11 @@ function errorHandlingMiddleware(error, req, res, next) {
   console.log("error handling middleware");
   console.log(error);
   // console.error(error);
-  if (error) {
+  if (error.status) {
+    console.log("88888888888888888888");
+    console.log("2222222222222222");
+    console.log(error);
+
     res.status(error.status).send({ message: error.message });
   } else {
     res.status(500).send({ message: error.toString() });
