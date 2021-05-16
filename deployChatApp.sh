fb7183b919891f28d2e79bc559dc2d896e111eac
Johnny Kung <kjohnathan.engi@gmail.com>
2021-05-15 21:06:56 -0700

    fixed script for chat app

--- deployChatApp.sh
+++ deployChatApp.sh
@@ -1,7 +1,7 @@
 echo "DEPLOYMENT STARTED";
-if [ -d "../idsp_townsquare" ] 
+if [ -d "../idsp2-chat" ] 
 then
-    rm -r ../idsp_townsquare;
+    rm -r ../idsp2-chat;
     cd ..;
     git clone git@github.com:kjohnathan/idsp2-chat.git;
     cd idsp2-chat;
