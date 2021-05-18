d25a5b39d7698cb59611f814502e71fba19a22be
Sean Luo <shawnluo415@gmail.com>
2021-05-18 16:59:13 -0700

    minor

--- src/model/conversation.model.ts
+++ src/model/conversation.model.ts
@@ -1,118 +1,136 @@
 import { getDB } from "../util/database.util";
 
 interface IUserInConversation {
-    userId: string,
-    username: string,
-    avatar: string
+  userId: string;
+  username: string;
+  avatar: string;
 }
 
 class ConversationModel {
-    private _db = getDB();
-    private _members: string[];
-    constructor(members) {
-        this._members = members;
-    }
+  private _db = getDB();
+  private _members: string[];
+  constructor(members) {
+    this._members = members;
+  }
 
-    public create = async () => {
-        const newConversation: { _id?: string, members: string[]} = {
-            members: this._members
-        };
-        await this._db.collection("conversation").insertOne(newConversation);
-        return newConversation;
-    }
+  public create = async () => {
+    const newConversation: { _id?: string; members: string[] } = {
+      members: this._members,
+    };
+    await this._db.collection("conversation").insertOne(newConversation);
+    return newConversation;
+  };
 
-    static getConversationByMembers = async (memberIds: string[]) => {
-        const database = getDB();
-        const result = await database.collection("conversation").find({
-            "members.userId": {
-                "$all": memberIds
-            }, 
-            members: { 
-                $size: memberIds.length
-            }
-        }).toArray();
-        /** result = conversation[] */
-        return result;
-    }
+  static getConversationByMembers = async (memberIds: string[]) => {
+    const database = getDB();
+    const result = await database
+      .collection("conversation")
+      .find({
+        "members.userId": {
+          $all: memberIds,
+        },
+        members: {
+          $size: memberIds.length,
+        },
+      })
+      .toArray();
+    /** result = conversation[] */
+    return result;
+  };
 
-    static getAllConversationsByUserId = async (userId) => {
-        const database = getDB();
-        const result = await database.collection("conversation").aggregate([
-            { $match: {"members.userId": userId }},
-            {
-                $project: { 
-                    members: {
-                        $filter: {
-                                input: "$members",
-                                as: "member",
-                                cond: {
-                                $ne: ["$$member.userId", userId ]
-                                }
-                        }
-                    },
-                    "conversationId": {
-                        $toString: "$_id"
-                    },
-                    _id: 0
-                }
-            }, 
-            {
-                $lookup: {
-                    from: "message",
-                    let: { "conversationId": "$conversationId" },
-                    pipeline: [
-                        {"$match":{"$expr":{"$eq":["$conversationId","$$conversationId"] } } },
-                        { $sort: { "createdAt": -1 } },
-                        { $limit: 1 },
-                    ],
-                    as: "messages"
-                }
+  static getAllConversationsByUserId = async (userId) => {
+    const database = getDB();
+    const result = await database
+      .collection("conversation")
+      .aggregate([
+        { $match: { "members.userId": userId } },
+        {
+          $project: {
+            members: {
+              $filter: {
+                input: "$members",
+                as: "member",
+                cond: {
+                  $ne: ["$$member.userId", userId],
+                },
+              },
             },
-            {
+            conversationId: {
+              $toString: "$_id",
+            },
+            _id: 0,
+          },
+        },
+        {
+          $lookup: {
+            from: "message",
+            let: { conversationId: "$conversationId" },
+            pipeline: [
+              {
                 $match: {
-                    "messages": { $exists: true }
-                }
-            }
-        ]).toArray();
-        const displayedConversations = result.filter(conversation => conversation.messages.length);
-        return displayedConversations;
-    }
+                  $expr: { $eq: ["$conversationId", "$$conversationId"] },
+                },
+              },
+              { $sort: { createdAt: -1 } },
+              { $limit: 1 },
+            ],
+            as: "messages",
+          },
+        },
+        {
+          $match: {
+            messages: { $exists: true },
+          },
+        },
+      ])
+      .toArray();
+    const displayedConversations = result.filter(
+      (conversation) => conversation.messages.length
+    );
+    return displayedConversations;
+  };
 
-    static getMessagesInConversation = async (conversationId) => {
-        const database = getDB();
-        const result = await database.collection("message").find({
-            conversationId
-        }).toArray();
-        return result;
-    }
+  static getMessagesInConversation = async (conversationId) => {
+    const database = getDB();
+    const result = await database
+      .collection("message")
+      .find({
+        conversationId,
+      })
+      .toArray();
+    return result;
+  };
 
-    static getUsersInConversation = async (members) => {
-        const database = getDB();
-        const result = await database.collection("user").aggregate([
-            {
-                "$addFields": {
-                    "userId": {
-                        "$toString": "$_id"
-                    }
-                }
+  static getUsersInConversation = async (members) => {
+    const database = getDB();
+    const result = await database
+      .collection("user")
+      .aggregate([
+        {
+          $addFields: {
+            userId: {
+              $toString: "$_id",
             },
-            {
-                "$match": {
-                    "userId": {
-                        "$in": members
-                    }
-                }
+          },
+        },
+        {
+          $match: {
+            userId: {
+              $in: members,
             },
-            {
-                "$project": {
-                    "userId": 1,
-                    "username": 1,
-                    "avatar": 1,
-                    "_id": 0
-                }
-            }
-        ]).toArray();
-        return result;
-    }
+          },
+        },
+        {
+          $project: {
+            userId: 1,
+            username: 1,
+            avatar: 1,
+            _id: 0,
+          },
+        },
+      ])
+      .toArray();
+    return result;
+  };
 }
 export default ConversationModel;
